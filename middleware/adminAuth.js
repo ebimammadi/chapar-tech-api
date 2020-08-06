8898e6a62c9b467214130f4b9216e0ff10dc529d
ebimammadi <ebi@tadbir.net>
2020-08-06 12:25:44 +0200

    403 message = ''

--- middleware/adminAuth.js
+++ middleware/adminAuth.js
@@ -1,13 +1,12 @@
 
 const jwt = require('jsonwebtoken')
-const _ = require('lodash')
 
 const adminAuth = (req, res, next) => {
 	const token = req.cookies["x-auth-token"]
-	if (!token) return res.status(403).json({ message: 'No valid token! Access denied.'})
+	if (!token) return res.status(403).json({ message: '' })
   const { userRole } = jwt.verify(token, process.env.JWT_KEY)
   if ( userRole !== "admin" ) 
-    return res.status(403).json({ message: `No Access, Access denied!` })
+    return res.status(403).json({ message: '' })
   next()
 }
 
--- middleware/auth.js
+++ middleware/auth.js
@@ -6,18 +6,18 @@ const { cookieOptions } = require("../middleware/cookie")
 
 const auth = async(req, res, next) => {
 	const token = req.cookies["x-auth-token"]
-	if (!token) return res.status(403).json({ message: 'No valid token! Access denied.'})
+	if (!token) return res.status(403).json({ message: ''})
 	try {
 		const decodedToken = jwt.verify(token, process.env.JWT_KEY)
 		const user = new User(_.pick( decodedToken, ['name','email','_id','userRole','profilePhotoUrl']))
 		const newToken = user.generateAuthToken(decodedToken.s)
 		const session = await refreshSession(decodedToken.s, newToken)		
-		if (!session) return res.status(403).json({ message: 'Login again.'})
+		if (!session) return res.status(403).json({ message: ''})
 		res.cookie('x-auth-token', newToken, cookieOptions)
 		next()
 	} 
 	catch (ex) {
-		return res.status(403).json({ message: 'Invalid token!'})
+		return res.status(403).json({ message: ''})
 	}
 }
 
