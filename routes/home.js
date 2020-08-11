6631c2d24628bf60816b9d22727fece61b21c08c
ebimammadi <ebi@tadbir.net>
2020-08-11 15:02:53 +0200

    todo contact-us-send-message

--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router(); 
 
 const auth = require('../middleware/auth')
+const mailer = require("../../components/nodemailer")
 
 router.get('/', auth, (req,res) => {
 	return res.json({ message: 'What are you looking for!' })
@@ -9,6 +10,13 @@ router.get('/', auth, (req,res) => {
 
 router.get('/about', auth, (req,res) => {
 	return res.json({ message: 'this is an application for optimizing parcel deliveries.' })
+})
+
+router.get('/contact-us-send-message', async (req,res) => {
+	//todo validate email mobile name, message
+	// save on db and show on app panel for the admins!
+ 	///await mailer(user.email, `'${user.userRole}' Access granted at ${process.env.APP_NAME}`, user, 'UsersRoleGranted')
+	return res.json({ message: 'mailed' })
 })
 
 module.exports = router
