f97bcf6cc4418f93025c73018dfc806cfcbaf283
ebimammadi <ebi@tadbir.net>
2020-08-12 12:37:58 +0200

    sameSite: 'Strict',

--- middleware/cookie.js
+++ middleware/cookie.js
@@ -6,7 +6,7 @@ const responseHeaderConfig = function (req, res, next) {
 
 const cookieOptions = { 
 	maxAge: parseInt(parseFloat(process.env.JWT_EXP_HOUR)*3600*1000), 
-	sameSite: 'none',
+	sameSite: 'Strict',
 	secure: true,
 	httpOnly: true
 }
