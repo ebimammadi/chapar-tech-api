e3317c9017bafc751a0d82ea9aebc08b23dd38cf
ebimammadi <ebi@tadbir.net>
2020-08-12 19:21:54 +0200

    /me added

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router(); 
-
+const jwt = require("jsonwebtoken")
+const _ =require("lodash")
+const { User } = require("../models/user")
 const auth = require('../middleware/auth')
-const mailer = require("../components/nodemailer")
-
+//const mailer = require("../components/nodemailer")
+const { refreshSession } = require("../middleware/session")
 router.get('/', auth, (req,res) => {
 	return res.json({ message: 'What are you looking for!' })
 })
@@ -17,6 +19,24 @@ router.get('/contact-us-send-message', async (req,res) => {
 	// save on db and show on app panel for the admins!
  	///await mailer(user.email, `'${user.userRole}' Access granted at ${process.env.APP_NAME}`, user, 'UsersRoleGranted')
 	return res.json({ message: 'mailed' })
+})
+
+router.get('/me', async (req, res) => {
+  try {
+		const token = req.cookies["x-auth-token"]
+		const decodedToken = jwt.verify(token, process.env.JWT_KEY)
+		const result = await refreshSession(decodedToken.s, token)
+		if (!result ) return res.send( {} )
+		const user = await User.findOne( { _id: decodedToken._id })
+		const me = _.pick( user, ["email", "name", "profilePhotoUrl", "userRole" ] )
+		if  ( !_.isEqual(me, _.pick( decodedToken, ["email", "name", "profilePhotoUrl", "userRole" ] ))) {
+			const newToken = user.generateAuthToken(decodedToken.s)
+			res.cookie('x-auth-token', newToken, cookieOptions)
+		}
+		return res.send(me)
+	} catch (_) {
+		return res.send( {} )
+	}
 })
 
 module.exports = router
