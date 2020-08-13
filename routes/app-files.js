5e1cea179a3d9c6f7f165944cc9f349769e9ba99
ebimammadi <ebi@tadbir.net>
2020-08-13 18:59:26 +0200

    refactored, handle S3 errors

--- routes/app-files.js
+++ routes/app-files.js
@@ -3,107 +3,97 @@ const router = express.Router()
 const Joi = require("joi")
 const sha256 = require("js-sha256")
 const jwt = require ("jsonwebtoken")
-
-const AWS = require("aws-sdk")
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ID,
-  secretAccessKey: process.env.AWS_SECRET
-})
-
+const isBase64 = require("is-base64")
+const winston = require("winston")
 const { User } = require("../models/user")
 const auth = require("../middleware/auth")
+const { uploadToS3, removeFromS3 } = require("../components/s3")
+
+const validPrefixes = ['profile','product','point','receipt']
+const validateUploadImage = (image) => {	
+	const imageSchema = Joi.object({
+		usage: Joi.string().required().valid(...validPrefixes),
+		unique: Joi.string().valid('','true'),
+		image: Joi.string().required(),
+		_id: Joi.objectId().allow('')
+	})
+	return imageSchema.validate(image)
+}
+const fileNameKeyGenerator = (usage, ownerId, _id) => {
+	return usage + '-'+ encodeHex(ownerId) + '-'+ encodeHex(_id) + '-' + sha256(_id+Date.now()) +'.jpg'
+}
+
 //TODO s3 error and fault handle!!!! both on the front-end ;)
-router.post('/upload-image', auth, async(req, res) => { //post payload: usage, unique, image
+router.post('/upload-image', auth, async(req, res) => { 
+	//post payload: usage, unique, image, _id
 	const { error } = validateUploadImage(req.body)
 	if (error) return res.json({ message: `${error.details[0].message}` })
-	try{
-		//generate-filename
-		const { _id } = jwt.verify( req.cookies["x-auth-token"], process.env.JWT_KEY)
-		//read record from database
-		const user = await User.findOne({ _id })
-		//const profilePhotoUrl = user.profilePhotoUrl
-		//remove previous file
-		if (req.body.unique === 'true' && user.profilePhotoUrl ) await removeFromS3(user.profilePhotoUrl)
-    //upload to storage
-    //!check req.body.image for an image check
-    //read the base64Img to buffer
-    const buffer = Buffer.from(req.body.image.replace(/^data:image\/\w+;base64,/, ""),'base64')
-    const fileNameKey = req.body.usage + '-'+ encodeHex(_id) + '-' + sha256(_id+Date.now()) +'.jpg';//!png and other extensions
-    //write the file to AWS S3 ;)
-    const { Location } = await uploadToS3( fileNameKey, buffer )
-		//save to database
-		user.set({ profilePhotoUrl: Location })
-		await user.save()
-		//return the result
-		res.status(200).json({
-			success: 'true',
-			url: Location
-		})
-	}catch(err){
-		return res.json({ response_type: 'warning', message: err.message })
+
+	const { _id } = jwt.verify( req.cookies["x-auth-token"], process.env.JWT_KEY)
+	//read record from database
+	const user = await User.findOne({ _id })
+	//remove previous file if required
+	if (req.body.unique === 'true' && user.profilePhotoUrl ) {
+		try{
+			await removeFromS3(user.profilePhotoUrl)
+		} catch (err){
+			return winston.log(err)
+		}
 	}
+	const fileNameKey = fileNameKeyGenerator(req.body.usage, _id, _id) 
+	//read the base64Img to buffer
+	if (!isBase64(req.body.image, { mime: true })) return res.json({ message: "Invalid image"})
+	
+	//!check req.body.image for an image check
+	const buffer = Buffer.from(req.body.image.replace(/^data:image\/\w+;base64,/, ""),'base64')
+	//write the file to AWS S3 ;)
+	const { Location } = await uploadToS3( fileNameKey, buffer )
+	//save to database
+	user.set({ profilePhotoUrl: Location })
+	await user.save()
+	return res.json({ response_type: 'success', url: Location, _id: _id })
+
+	// }catch(err){
+	// 	return res.json({ response_type: 'warning', message: err.message })
+	// }
 })
-//!
+
 router.get('/delete-image/:server/:filename', auth, async(req, res) => {
-	try{
-		const filename = `https://${req.params.server}/${req.params.filename}`
-		const [ usage, encodedId ] = req.params.filename.split("-")
-		//TODO It can be developed for other usages types 
-		if(usage !== "profile") return res.json({ message: `this usage '${usage}' is not allowed yet!`})
-		let { _id } = jwt.verify(req.cookies["x-auth-token"], process.env.JWT_KEY)
-		
-		if (_id != decodeHex(encodedId) ) return res.json({ message: `You are not allowed to delete this file!`})
-		_id = decodeHex(encodedId)
+
+	const filename = `https://${req.params.server}/${req.params.filename}`
+	const [ usage, encodedOwnerId, encodedId ] = req.params.filename.split("-")
+	if(!validPrefixes.includes(usage)) return res.json({ message: `Invalid filename!`})
+	
+	//check file owner
+	let { _id } = jwt.verify(req.cookies["x-auth-token"], process.env.JWT_KEY)
+	if (_id != decodeHex(encodedOwnerId) ) 
+		return res.json({ message: `You are not allowed to delete this file!`})
+
+	//check file validity// filename exist on aws
+	_id = decodeHex(encodedOwnerId)
+	
+	if (usage === "profile") {
 		const user = await User.findOne({ _id })
-		if (!user || (user.profilePhotoUrl != filename) ) return res.json({ message: `invalid file! ${filename}` })
-
-		//delete file from server
-		const result = await removeFromS3(filename)
-
-		//update filename in db
-		user.set({ profilePhotoUrl: '' })
-		await user.save();	
-		//return
-		return res.json({ response_type: 'success', url: filename, message: `${filename} removed successfully.` })
-	}catch(err){
-		
-		return res.json({ message: err.message })
+		if (!user || (user.profilePhotoUrl != filename) ) 
+			return res.json({ message: `invalid file! ${filename}` })
 	}
-
+	
+	//delete file from server
+	try{
+		await removeFromS3(filename)
+	} catch (err){
+		return winston.log(err, 'error')
+	}
+	
+	//update database
+	if (usage=== "profile") {
+		await User.findByIdAndUpdate(_id, {profilePhotoUrl: ''})		
+	}
+	
+	return res.json({ response_type: 'success', url: filename, message: `${filename} removed successfully.` })
 })
 
-const removeFromS3 = (file) => {
-  const [ key ] = file.split('/').reverse()
-  const s3Params = {
-    Bucket: process.env.AWS_BUCKET_NAME,
-    Key: key
-  };
-	return new Promise ( (resolve, reject) => {
-    s3.deleteObject(s3Params, function(err, data) {
 
-      if (err) {
-				console.log(`error`,err.stack)
-				reject (err.stack)
-			}
-			else resolve(data)
-    })
-	})
-}
-
-const uploadToS3 = (key, body) => {
-  const s3Params = {
-    Bucket: process.env.AWS_BUCKET_NAME,
-    Key: key,
-    Body: body,
-    ACL: "public-read"
-  }
-  return new Promise( (resolve, reject) => {
-    s3.upload(s3Params, (error,data) => {
-      if (error) reject(error)
-      else resolve(data)
-    })
-  })
-}
 
 const toHex = (char = '') => char.charCodeAt(0).toString(16)
 const encodeHex = (str = '') => str.split('').map(toHex).join('')
@@ -115,15 +105,5 @@ const decodeHex = (hex = '') => {
 	return result.join('')
 }
 
-const validPrefixes = ['profile','product','point','receipt']
-
-const validateUploadImage = (image) => {	
-	const imageSchema = Joi.object({
-		usage: Joi.string().required().valid(...validPrefixes),
-		unique: Joi.string().valid('','true'),
-		image: Joi.string().required()
-	})
-	return imageSchema.validate(image)
-}
 
 module.exports = router
