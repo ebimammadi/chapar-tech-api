25bff657f842591cfa6f6e7f020875608f42057f
ebimammadi <ebi@tadbir.net>
2020-08-13 11:40:06 +0200

    improved; log to db configured

--- startup/logging.js
+++ startup/logging.js
@@ -1,24 +1,42 @@
-const winston = require("winston") // TODO: improve log reporting on an external file or mongodb storage
-//require("winston-mongodb")
+const winston = require("winston") 
+require("winston-mongodb")
 require("express-async-errors")
+
+const connectionOptions = { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+}
 
 module.exports = function() {
 
   process.on('uncaughtException', (ex) => {
-    //ew winston.transports.Console( { colorize: true, prettyPrint: true })
-    new winston.transports.File( { filename: 'logs-uncaught-unhandled.log' })
-    console.log("We got an UNCAUGHT exception:", ex)
-    //winston.log("We got an UNCAUGHT exception:", ex)
+    new winston.transports.Console({ colorize: true, prettyPrint: true })
+    new winston.transports.File({ filename: 'log-file.log' })
+    new winston.transports.MongoDB({ 
+      db: process.env.DB_CONNECT, 
+      options: connectionOptions, 
+      level: 'silly' 
+    })
+    winston.log("We got an UNCAUGHT exception:", ex)
   })
 
   process.on('unhandledException', (ex) => {
-    //new winston.transports.Console( { colorize: true, prettyPrint: true })
-    new winston.transports.File( { filename: 'logs-uncaught-unhandled.log' })
-    console.log("We got an UNHANDLED exception:", ex)
-    // winston.log("We got an UNHANDLED exception:", ex)
+    new winston.transports.Console({ colorize: true, prettyPrint: true })
+    new winston.transports.File({ filename: 'log-file.log' })
+    new winston.transports.MongoDB({ 
+      db: process.env.DB_CONNECT, 
+      options: connectionOptions, 
+      level: 'silly' 
+    })
+    winston.log("We got an UNHANDLED exception:", ex)
   })
 
-  winston.add( new winston.transports.File( { filename: 'log-file.log' }) )
-  //winston.add( winston.transports.MongoDB, { db: process.env.DB_CONNECT, level: 'silly'} )
+  winston.add( new winston.transports.Console({ colorize: true, prettyPrint: true }) )
+  winston.add( new winston.transports.File({ filename: 'log-file.log' }) )
+  winston.add( new winston.transports.MongoDB({ 
+    db: process.env.DB_CONNECT, 
+    options: connectionOptions,
+    level: 'silly' 
+  }))
 
 }
