8e1806998fcd09319fd502d67e5279b20275fc24
ebimammadi <ebi@tadbir.net>
2020-08-13 18:58:40 +0200

    refactor

--- middleware/error.js
+++ middleware/error.js
@@ -1,7 +1,9 @@
 const winston = require("winston")
 
 module.exports = (err, req, res, next) => {
-	console.log(err)
 	winston.error(err.message, err)
-  res.status(500).send({ response_type: 'warning', message: `Error on server!`})
+  res.status(500).send({ 
+		response_type: 'warning', 
+		message: `Error on server!`, 
+		more_info: err.message.slice(0,100)+'..., for more info, check logs!' })
 }
