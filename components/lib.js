1cc805f242fcb032a56c4cee017f2fcafc98d07d
ebimammadi <ebi@tadbir.net>
2020-08-14 13:40:42 +0200

    encodeHex decodeHex

--- components/lib.js
+++ components/lib.js
@@ -34,4 +34,18 @@ const utcNow = () => {
   return now.toISOString()
 }
 
-module.exports = { regexUrl, regexSlug, escapeText, regex, validateURL, utcNow }
+const toHex = (char = '') => char.charCodeAt(0).toString(16)
+const encodeHex = (str = '') => str.split('').map(toHex).join('')
+const decodeHex = (hex = '') => {
+	const result = []
+	for (let i = 0; i < hex.length; i += 2) {
+		result.push(String.fromCharCode(parseInt(hex.substr(i, 2), 16)))
+	}
+	return result.join('')
+}
+
+module.exports = { 
+  regexUrl, regexSlug, escapeText, regex, validateURL, utcNow,
+  encodeHex,
+  decodeHex
+}
