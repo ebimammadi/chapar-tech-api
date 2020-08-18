851e64018111d2805b715d3f5c9549ab1cdead80
ebimammadi <ebi@tadbir.net>
2020-08-18 10:16:14 +0200

    refactor

--- routes/app-products.js
+++ routes/app-products.js
@@ -1,42 +1,47 @@
 const express = require("express")
-const router = express.Router()
-const _ =require("lodash")
+const _ = require("lodash")
+const winston = require("winston")
 const jwt = require("jsonwebtoken")
 const { regex } = require("../components/lib")
 const { Product, validateProduct, generateProductSlug } = require("../models/product")
 const { auth, supplierAuth } = require("../middleware/auth")
 
+const { removeFromS3 } = require("../components/s3")
+const router = express.Router()
+
 router.post('/product-set', auth, supplierAuth, async (req,res) => {
-  // validate post payload (name, slug, description, features, _id)
+  // validate post payload (_id, name, slug, description, price)
   const { error } = validateProduct.productSet(req.body)
   if (error) return res.json({ message: error.details[0].message })
 
   const token = jwt.verify( req.cookies["x-auth-token"], process.env.JWT_KEY)
-
-  //if productId is set
-  if (req.body._id == "") {
+  if (req.body._id == "" || req.body._id === undefined ) {
     const slug = await generateProductSlug(req.body.slug)
     const product = new Product({ 
-      ..._.pick(req.body, ['name', 'description', 'features','images']), 
+      ..._.pick(req.body, ['name', 'description','price','images']), 
       slug, 
       ownerId: token._id,
       ownerSlug: token.slug,
       ownerName: token.name 
     })
     await product.save()
-    return res.send( {response_type: `success`, productId: product._id, message: `Product created successfully.`})
+    return res.send( {response_type: `success`, _id: product._id, message: `Product created successfully.`})
+  } else {
+    const product = await Product.findById(req.body._id) 
+    if (!product) return res.send({ message: 'Invalid product Id' })
+    if (product.ownerId.toString() !== token._id) return res.send({ message: 'You are not allowed to change this product.' })
+    if (product.slug === req.body.slug) product.slug = req.body.slug
+    else product.slug = await generateProductSlug(req.body.slug, req.body._id)
+    product.name = req.body.name
+    product.description = req.body.description
+    product.price = req.body.price
+    product.images = req.body.images
+    product.ownerSlug = token.slug
+    product.ownerName = token.name
+    product.updated_at = Date.now()
+    await product.save()
+    return res.send( {response_type: `success`, _id: product._id, message: `Product saved successfully.`})
   }
-  const product = await Product.findById(req.body._id) 
-  if (!product) return res.send({ message: 'Invalid product Id' })
-  if (product.ownerId.toString() !== token._id) return res.send({ message: 'You are not allowed to change this product.' })
-  if (product.slug === req.body.slug) product.slug = req.body.slug
-  else product.slug = await generateProductSlug(req.body.slug)
-  product.name = req.body.name
-  product.description = req.body.description
-  product.features = product.features
-  product.ownerSlug = token.slug
-  await product.save()
-  return res.send( {response_type: `success`, productId: product._id, message: `Product saved successfully.`})
 })
 
 router.post('/product-delete', auth, supplierAuth, async (req, res) => {
@@ -46,9 +51,16 @@ router.post('/product-delete', auth, supplierAuth, async (req, res) => {
   const token = jwt.verify( req.cookies["x-auth-token"], process.env.JWT_KEY)
   const product = await Product.findById(req.body._id) 
   if (!product) return res.send({ message: 'Invalid product Id.' })
-  if (product.ownerId.toString() !== token._id) return res.send({ message: 'You are not allowed to change this product.' })
+  if (product.ownerId.toString() !== token._id && token.userRole!= "admin") return res.send({ message: 'You are not allowed to change this product.' })
+  product.images.forEach(async function(image) {
+    try {
+      await removeFromS3(image)
+    } catch (err) {
+      return winston.log(`error`, 'Error:', err)
+    }
+  })
   await product.remove()
-  return res.send({ response_type: "success", message: "" })  
+  return res.send({ response_type: "success", message: `Product '${product.slug}' has been deleted successfully.` })  
 })
 
 router.post('/product-publish', auth, supplierAuth, async (req, res) => {
@@ -64,9 +76,11 @@ router.get('/product-get/:_id', auth, supplierAuth, async (req, res) => {
   const product = await Product.findById(req.params._id) 
   if (!product) return res.send({ message: 'Invalid product Id.' })
   if (product.ownerId.toString() !== token._id) return res.send({ message: 'You are not allowed to view this product.' })
+  let resultProduct = _.pick( product, ["name","slug","description","_id","images","price","ownerName","ownerSlug"])
+  resultProduct = _.defaults( resultProduct, { description: '',ownerName: '', ownerSlug: '', price: '1' } )
   return res.send({ 
     response_type: "success", 
-    product: _.pick( product, ["name","slug","description","_id","features","images"]) 
+    product: resultProduct
   })  
 })
 
@@ -113,7 +127,7 @@ router.get('/product-list', auth, supplierAuth, async (req, res) => {
   const products = result[0].data.map ( product => 
     _.defaults( 
       _.omit( product, ["__v"]), 
-      { description: '',ownerName: '', ownerSlug: '', publishStatus: 'false' } 
+      { description: '',ownerName: '', ownerSlug: '', price: '1', publishStatus: 'false' } 
     )
   )
   return res.json( { perPage, count: result[0].count, products })
